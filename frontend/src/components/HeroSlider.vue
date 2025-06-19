72498ec9d5684384d47c61baaf36c95e5191c3a7
Gerard Salao <ghsalao@up.edu.ph>
2025-06-19 11:01:13 +0800

    feat(frontend): add 'Book Now' button

--- frontend/src/components/HeroSlider.vue
+++ frontend/src/components/HeroSlider.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="relative h-screen w-full text-white z-10">
+  <section class="relative h-[60vh] w-full text-white z-10 flex items-center justify-center">
     <!-- Background Image -->
     <div
       class="absolute inset-0 bg-cover bg-center z-0"
@@ -9,20 +9,23 @@
     <!-- Overlay -->
     <div class="absolute inset-0 bg-black opacity-80 z-10"></div>
 
-    
     <!-- Hero Content -->
-    <div class="relative z-20 flex flex-col items-center justify-center text-center h-full px-4">
+    <div class="relative z-20 flex flex-col items-center justify-center text-center px-4">
       <h1 class="text-5xl md:text-7xl font-bold text-yellow-500 drop-shadow-lg">UP NISMED Hostel</h1>
       <p class="mt-4 text-lg md:text-2xl text-yellow-400 font-sm drop-shadow-lg">
         The Hostel provides a quiet retreat for its guests, a second-home ambience, and a haven for the professional and transients.
-        
-        <!-- TODO: insert more info; uncomment following txt: -->
-        <!-- The hostel has forty-eight (48) rooms and can accommodate a maximum of 120 guests. Each room is equipped with a telephone. Free Wi-Fi is also available. -->
       </p>
+
+      <!-- Book Now Button -->
+      <button
+        class="cursor-pointer mt-15 px-8 py-4 bg-yellow-500 text-black font-bold text-2xl rounded-lg shadow-lg hover:bg-yellow-600 transition-all duration-300"
+      >
+        Book Now
+      </button>
     </div>
   </section>
 </template>
 
 <script setup>
-import nismedHero from '@/assets/images/nismed-hostel-landing.jpg'
+  import nismedHero from '@/assets/images/nismed-hostel-landing.jpg'
 </script>
