cda016ca4008043181447bfb53881efbf211dec9
Harry Q <87745547+Harry2166@users.noreply.github.com>
2025-06-19 11:09:31 +0800

    feat: edit UI of the admin panel

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -140,4 +140,48 @@ JAZZMIN_SETTINGS = {
     "search_model": ["room.room"],
     "hide_models": ["auth.group"],
     "order_with_respect_to": ["reservation", "guest", "room", "auth"],
+    "icons": {
+        "auth.user": "fas fa-user",
+        "guest.guest": "fas fa-users",
+        "reservation.reservation": "fas fa-hotel",
+        "room.room": "fas fa-bed",
+        "room.roomrate": "fas fa-dollar-sign",
+        "room.roomtype": "fas fa-door-open", 
+    },
+    "show_sidebar": True,
+    # "show_ui_builder": True,
+    # "related_modal_active": True
 }
+
+JAZZMIN_UI_TWEAKS = {
+    "navbar_small_text": False,
+    "footer_small_text": False,
+    "body_small_text": False,
+    "brand_small_text": False,
+    "brand_colour": False,
+    "accent": "accent-olive",
+    "navbar": "navbar-dark",
+    "no_navbar_border": False,
+    "navbar_fixed": False,
+    "layout_boxed": False,
+    "footer_fixed": False,
+    "sidebar_fixed": True,
+    "sidebar": "sidebar-dark-olive",
+    "sidebar_nav_small_text": False,
+    "sidebar_disable_expand": False,
+    "sidebar_nav_child_indent": False,
+    "sidebar_nav_compact_style": False,
+    "sidebar_nav_legacy_style": False,
+    "sidebar_nav_flat_style": False,
+    "theme": "default",
+    "dark_mode_theme": None,
+    "button_classes": {
+        "primary": "btn-primary",
+        "secondary": "btn-secondary",
+        "info": "btn-info",
+        "warning": "btn-warning",
+        "danger": "btn-danger",
+        "success": "btn-success"
+    }
+}
+
