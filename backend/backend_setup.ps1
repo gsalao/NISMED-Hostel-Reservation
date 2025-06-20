ebc7b22c9041084a9b2d208b398f3402aebef720
Harry2166 <harry2166.hq@gmail.com>
2025-06-20 17:57:27 +0800

    chore: create powershell script

---
+++ backend/backend_setup.ps1
@@ -0,0 +1,12 @@
+.\env\Scripts\activate
+pip install -r requirements.txt
+python manage.py makemigrations
+python manage.py migrate
+python manage.py createsuperuser
+python manage.py loaddata fixtures/guest.json
+python manage.py loaddata fixtures/room_type.json
+python manage.py loaddata fixtures/room_room.json
+python manage.py loaddata fixtures/room_rate.json
+python manage.py loaddata fixtures/reservation_reservation.json
+python manage.py loaddata fixtures/reservation_reservedrooms.json
+python manage.py runserver
