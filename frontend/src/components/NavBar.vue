1ae5c3efbfc4c18deac6e2a9e162235f3174bd2b
Harry Q <87745547+Harry2166@users.noreply.github.com>
2025-07-03 13:02:22 +0800

    feat: make the top left into a button that leads back to home

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -40,10 +40,10 @@
 <template>
   <nav class="fixed top-0 w-full z-30 bg-[#09240B]">
     <div class="max-w-screen-xl mx-auto px-4 py-3 flex justify-between items-center text-white">
-      <span class="font-bold text-xl md:text-2xl flex items-center space-x-2">
+      <button @click="goToHome" class="font-bold text-xl md:text-2xl flex items-center space-x-2">
         <img :src="nismedLogo" alt="UP NISMED Logo" class="h-10 w-auto">
         <span>UP NISMED Hostel</span>
-      </span>
+      </button>
       <ul class="flex space-x-4 text-lg md:text-2xl">
         <li>
           <button @click="goToHome" class="cursor-pointer hover:underline">Home</button>
