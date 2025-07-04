b1bb397682795b843d172f63e2de0e83b1ef6409
Harry Q <87745547+Harry2166@users.noreply.github.com>
2025-07-04 11:04:44 +0800

    feat: add get request for room type images

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -230,3 +230,6 @@ CACHES = {
         }
     }
 }
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
--- backend/room/serializers.py
+++ backend/room/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Room, RoomType, RoomRate, Amenity
+from .models import Room, RoomType, RoomRate, Amenity, RoomTypeImage
 
 class AmenitySerializer(serializers.ModelSerializer):
     class Meta:
@@ -29,3 +29,8 @@ class RoomRateSerializer(serializers.ModelSerializer):
         fields = '__all__' 
     def get_str(self, obj):
         return str(obj)
+
+class RoomTypeImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = RoomTypeImage
+        fields = ['id', 'name', 'image', 'room_type']
