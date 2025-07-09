c542b28e56e3f0908889ca7f82f0df9e5dd0846e
Harry Q <87745547+Harry2166@users.noreply.github.com>
2025-07-09 12:22:40 +0800

    chore: remove print

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -27,7 +27,6 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
-print(ALLOWED_HOSTS)
 
 # Application definition
 
